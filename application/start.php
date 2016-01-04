a59f14af64013a515fa16a09cbf63bda7392f616
control-corp <control.corp@abv.bg>
2016-01-04 19:17:36 +0200

    test

--- application/start.php
+++ application/start.php
@@ -6,7 +6,7 @@ use Micro\Application\Router;
 require 'library/Micro/autoload.php';
 
 if ((file_exists($classes = 'application/data/classes.php')) === \true) {
-    MicroLoader::setFiles(include $classes);
+    \MicroLoader::setFiles(include $classes);
 }
 
 $app = new Application(include 'application/config/app.php');
