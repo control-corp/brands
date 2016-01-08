fd793ff5fdcdc5088fab5a71c936640617a6ee67
control-corp <control.corp@abv.bg>
2016-01-08 12:21:27 +0200

    comments

--- application/start.php
+++ application/start.php
@@ -6,22 +6,34 @@ use Micro\Session\Session;
 
 require 'library/Micro/autoload.php';
 
+/**
+ * Cached mapped classes
+ */
 if ((file_exists($classes = 'application/data/classes.php')) === \true) {
     \MicroLoader::setFiles(include $classes);
 }
 
 $app = new Application(include 'application/config/app.php');
 
+/**
+ * Create router with routes
+ */
 $app['router'] = function () use ($app) {
     $router = new Router($app['request']);
     $router->mapFromConfig(include 'application/config/routes.php');
     return $router;
 };
 
+/**
+ * Add custom services per project
+ */
 foreach (\glob('application/config/dependencies/*.php') as $file) {
     include $file;
 }
 
+/**
+ * Register session config
+ */
 Session::register($app['config']->get('session', []));
 
 return $app;
