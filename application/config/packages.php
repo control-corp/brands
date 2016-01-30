ef8d794de59f7f3797ae4c748d73d7fc215f8f7d
control-corp <control.corp@abv.bg>
2016-01-30 20:02:14 +0200

    auto load packages

--- application/config/packages.php
+++ application/config/packages.php
@@ -1,14 +1,13 @@
 <?php
 
+$packages = [];
+
+foreach (new \FilesystemIterator('application/packages') as $f) {
+    $packages[$f->getFilename()] = $f->getPathname();
+}
+
 return [
-    'packages' => [
-        'App' => 'application/packages/App',
-        'Pages' => 'application/packages/Pages',
-        'Brands' => 'application/packages/Brands',
-        'Nomenclatures' => 'application/packages/Nomenclatures',
-        'UserManagement' => 'application/packages/UserManagement',
-        'Navigation' => 'application/packages/Navigation',
-        'Mail' => 'application/packages/Mail',
+    'packages' => $packages + [
         'MicroDebug' => 'library/Micro/packages/MicroDebug',
     ]
 ];
