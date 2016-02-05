2b91bf6b8fcb6c0c17ab85b985f442ecc23cb337
control-corp <control.corp@abv.bg>
2016-02-05 06:55:14 +0200

    Modified   application/packages/App/Resources/views/index/index.phtml Modified   application/start.php

--- application/start.php
+++ application/start.php
@@ -1,24 +1,28 @@
 <?php
 
 use Micro\Application\Application;
+use Micro\Application\Utils;
 
 include_once 'library/Micro/autoload.php';
 
-if (file_exists('vendor/autoload.php')) {
-    include_once 'vendor/autoload.php';
+if (is_file($composer = 'vendor/autoload.php')) {
+    include_once $composer;
 }
 
-/**
- * Cached mapped classes / dirs
- */
-if ((file_exists($classes = 'data/classes.php')) === \true) {
-    \MicroLoader::setFiles(include $classes);
+MicroLoader::register();
+
+if ((is_file($classes = 'data/classes.php')) === \true) {
+    MicroLoader::setFiles(include $classes);
 }
 
 $config = [];
 
 foreach (glob('{application/config/*.php,application/config/packages/*.php}', GLOB_BRACE) as $file) {
-    $config = array_merge($config, include $file);
+    $config = Utils::merge($config, include $file);
+}
+
+if (isset($config['packages'])) {
+    MicroLoader::addPath($config['packages']);
 }
 
 $app = new Application($config);
